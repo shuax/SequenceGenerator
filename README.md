0fdedcc69d9ecc39eb9c7060e1f01228e6fa4101
shuax <admin@shuax.com>
2015-06-04 13:07:51 +0800

    改变使用方式

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 因为经常要写配置表之类的操作，需要自动生成序号，于是写了这个Sublime Text插件。
 
+按下快捷键或者选择菜单以后，输入格式即可自动填充。
+
+支持两种格式 {起始值}, {起始值:步长}
+
 下面是演示过程：
 
 ![image](https://raw.githubusercontent.com/shuax/SequenceGenerator/master/demo.gif)
--- SequenceGenerator.py
+++ SequenceGenerator.py
@@ -2,12 +2,12 @@ import sublime, sublime_plugin
 
 class SequenceGeneratorCommand(sublime_plugin.TextCommand):
     def run(self, edit):
-        sublime.status_message("You could set the sequence format to: {start:end:step} or {start:end} / {end}.")
-        self.view.window().show_input_panel("Enter Sequence Format:", "10", self.on_done, self.on_change, None)
+        sublime.status_message("You could set the sequence format to: {start:step} or {start}.")
+        self.view.window().show_input_panel("Enter Sequence Format:", "1", self.on_done, self.on_change, None)
     def on_done(self, text):
         parameter = self.VerifyFormat(text)
         if not parameter:
-            sublime.status_message("Your input is invalid.")
+            sublime.error_message("Your input is invalid.")
         else:
             self.view.run_command("generate_sequence", {"parameter": parameter})
 
@@ -15,63 +15,40 @@ class SequenceGeneratorCommand(sublime_plugin.TextCommand):
         if not self.VerifyFormat(text):
             sublime.status_message("Your input is invalid.")
         else:
-            sublime.status_message("You could set the sequence format to: {start:end:step} or {start:end} / {end}.")
+            sublime.status_message("You could set the sequence format to: {start:step} or {start}.")
     def VerifyFormat(self, text):
         splits = text.replace(":", " ").split()
         length = len(splits)
-        if not 1<=length<=3:
+        if not 1<=length<=2:
             return False
         for number in splits:
-            if not number.isdigit():
+            try:
+                number = int(number)
+            except:
                 return False
-        
-        parameter = {'start':1, 'end':1, 'step':1}
+
+        parameter = {'start':1, 'step':1}
 
         if length==1:
-            parameter['end'] = int(splits[0]) + 1
-        elif length==2:
             parameter['start'] = int(splits[0])
-            parameter['end'] = int(splits[1]) + 1
         else:
             parameter['start'] = int(splits[0])
-            parameter['end'] = int(splits[1]) + 1
-            parameter['step'] = int(splits[2])
+            parameter['step'] = int(splits[1])
 
         return parameter
 
 class GenerateSequenceCommand(sublime_plugin.TextCommand):
-
-    def prepare_padding(self, edit, start):
-        def append(string):
-            string = str(string)
-            self.view.insert(edit, start[0], string)
-            start[0] += len(string)
-            return sublime.Region(start[0]-len(string), start[0])
-        return append
-
-    def generate(self, edit, region, sequence, new_sel):
-        contents = self.view.substr(region)
-        self.view.erase(edit, region)
-        padding_append = self.prepare_padding(edit, [region.begin()])
-        for i, x in enumerate(sequence):
-            splits = contents.split('#')
-            for j, s in enumerate(splits):
-                padding_append(s)
-                if j!=len(splits)-1:
-                    new_sel.append(padding_append(x))
-            if i!=len(sequence)-1:
-                padding_append('\n')
-
     def run(self, edit, parameter):
-        sequence = list(range(parameter['start'], parameter['end'], parameter['step']))
         new_sel = []
-
         for region in self.view.sel():
-            if region.empty():
-                line = self.view.line(region)
-                self.generate(edit, line, sequence, new_sel)
-            else:
-                self.generate(edit, region, sequence, new_sel)
+            self.view.erase(edit, region)
+
+            sequence = str(parameter['start'])
+            self.view.insert(edit, region.begin(), sequence)
+
+            new_sel.append(sublime.Region(region.begin(), region.begin() + len(sequence)))
+
+            parameter['start'] += parameter['step']
 
         if len(new_sel) > 0:
             self.view.sel().clear()
